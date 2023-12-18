b148737bae62fd90363a37b7cc22bbad7199672f
alexandervoitsekhovich <sashav0itsekhovich@gmail.com>
2023-12-18 19:14:32 +0300

    looks like i still have hope...

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from PyQt5 import uic
 from PyQt5.QtWidgets import *
+from PyQt5.QtCore import pyqtSignal, QEvent, QObject, QPoint
 import numpy as np
 from PyQt5.QtCore import Qt
 import sys
 import random
-import pprint
 
 
 def except_hook(cls, exception, traceback):
@@ -48,6 +48,8 @@ class StartPage(QMainWindow):
     def run_eight(self):
         self.window_eight = Eight()
         self.window_eight.show()
+        helper = MouseHelper(self.window_eight.windowHandle())
+        helper.pressed.connect(lambda point: self.window_eight.change_board(point.x(), point.y()))
 
     def run_fifteen(self):
         self.window_fifteen = Fifteen()
@@ -61,6 +63,28 @@ class StartPage(QMainWindow):
                 self.run_eight()
             else:
                 self.run_fifteen()
+
+
+class MouseHelper(QObject):
+    pressed = pyqtSignal(QPoint)
+    released = pyqtSignal(QPoint)
+
+    def __init__(self, window):
+        super().__init__(window)
+        self._window = window
+        self.window.installEventFilter(self)
+
+    @property
+    def window(self):
+        return self._window
+
+    def eventFilter(self, obj, event):
+        if self.window is obj:
+            if event.type() == QEvent.MouseButtonPress:
+                self.pressed.emit(event.pos())
+            elif event.type() == QEvent.MouseButtonRelease:
+                self.released.emit(event.pos())
+        return super().eventFilter(obj, event)
 
 
 class Eight(QWidget):
@@ -110,8 +134,8 @@ class Eight(QWidget):
         self.button22.clicked.connect(self.change_board)
         del nums[nums.index(str(self.button22.text()))]
 
-    def change_board(self):
-        ...
+    def change_board(self, x, y):
+        print(x, y)
 
 
 class Fifteen(QWidget):
@@ -127,72 +151,72 @@ class Fifteen(QWidget):
         board = np.zeros(16, dtype=np.int32).reshape(4, 4)
         self.button00.setText(random.choice(nums))
         board[0, 0] = int(self.button00.text()) if self.button00.text() != "" else 0
-        self.button00.clicked.connect(self.change_board)
+        self.button00.clicked.connect(self.getPos)
         del nums[nums.index(str(self.button00.text()))]
         self.button01.setText(random.choice(nums))
         board[0, 1] = int(self.button01.text()) if self.button01.text() != "" else 0
-        self.button01.clicked.connect(self.change_board)
+        self.button01.clicked.connect(self.getPos)
         del nums[nums.index(str(self.button01.text()))]
         self.button02.setText(random.choice(nums))
         board[0, 2] = int(self.button02.text()) if self.button02.text() != "" else 0
-        self.button02.clicked.connect(self.change_board)
+        self.button02.clicked.connect(self.getPos)
         del nums[nums.index(str(self.button02.text()))]
         self.button10.setText(random.choice(nums))
         board[1, 0] = int(self.button10.text()) if self.button10.text() != "" else 0
-        self.button10.clicked.connect(self.change_board)
+        self.button10.clicked.connect(self.getPos)
         del nums[nums.index(str(self.button10.text()))]
         self.button11.setText(random.choice(nums))
         board[1, 1] = int(self.button11.text()) if self.button11.text() != "" else 0
-        self.button11.clicked.connect(self.change_board)
+        self.button11.clicked.connect(self.getPos)
         del nums[nums.index(str(self.button11.text()))]
         self.button12.setText(random.choice(nums))
         board[1, 2] = int(self.button12.text()) if self.button12.text() != "" else 0
-        self.button12.clicked.connect(self.change_board)
+        self.button12.clicked.connect(self.getPos)
         del nums[nums.index(str(self.button12.text()))]
         self.button20.setText(random.choice(nums))
         board[2, 0] = int(self.button20.text()) if self.button20.text() != "" else 0
-        self.button20.clicked.connect(self.change_board)
+        self.button20.clicked.connect(self.getPos)
         del nums[nums.index(str(self.button20.text()))]
         self.button21.setText(random.choice(nums))
         board[2, 1] = int(self.button21.text()) if self.button21.text() != "" else 0
-        self.button21.clicked.connect(self.change_board)
+        self.button21.clicked.connect(self.getPos)
         del nums[nums.index(str(self.button21.text()))]
         self.button22.setText(random.choice(nums))
         board[2, 2] = int(self.button22.text()) if self.button22.text() != "" else 0
-        self.button22.clicked.connect(self.change_board)
+        self.button22.clicked.connect(self.getPos)
         del nums[nums.index(str(self.button22.text()))]
         self.button30.setText(random.choice(nums))
         board[3, 0] = int(self.button30.text()) if self.button30.text() != "" else 0
-        self.button30.clicked.connect(self.change_board)
+        self.button30.clicked.connect(self.getPos)
         del nums[nums.index(str(self.button30.text()))]
         self.button31.setText(random.choice(nums))
         board[3, 1] = int(self.button31.text()) if self.button31.text() != "" else 0
-        self.button31.clicked.connect(self.change_board)
+        self.button31.clicked.connect(self.getPos)
         del nums[nums.index(str(self.button31.text()))]
         self.button32.setText(random.choice(nums))
         board[3, 2] = int(self.button32.text()) if self.button32.text() != "" else 0
-        self.button31.clicked.connect(self.change_board)
+        self.button31.clicked.connect(self.getPos)
         del nums[nums.index(str(self.button32.text()))]
         self.button33.setText(random.choice(nums))
         board[3, 3] = int(self.button33.text()) if self.button33.text() != "" else 0
-        self.button33.clicked.connect(self.change_board)
+        self.button33.clicked.connect(self.getPos)
         del nums[nums.index(str(self.button33.text()))]
         self.button23.setText(random.choice(nums))
         board[2, 3] = int(self.button23.text()) if self.button23.text() != "" else 0
-        self.button23.clicked.connect(self.change_board)
+        self.button23.clicked.connect(self.getPos)
         del nums[nums.index(str(self.button23.text()))]
         self.button13.setText(random.choice(nums))
         board[1, 3] = int(self.button13.text()) if self.button13.text() != "" else 0
-        self.button13.clicked.connect(self.change_board)
+        self.button13.clicked.connect(self.getPos)
         del nums[nums.index(str(self.button13.text()))]
         self.button03.setText(random.choice(nums))
         board[0, 3] = int(self.button03.text()) if self.button03.text() != "" else 0
-        self.button03.clicked.connect(self.change_board)
+        self.button03.clicked.connect(self.change_board())
         del nums[nums.index(str(self.button03.text()))]
 
     def change_board(self, event):
-        if event.key() == Qt.key_W:
-            ...
+        print(event)
+        self.getPos(event)
 
 
 if __name__ == '__main__':
